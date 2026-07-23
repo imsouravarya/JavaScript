d9b0b61eaaca6a814af7ca15e5c27b49dc1ace08
imsouravarya <hellosouravarya@gmail.com>
2026-07-24 00:37:20 +0530

    more js

---
+++ switch/Readme.txt
@@ -0,0 +1,19 @@
+Switch
+
+This folder is about the switch statement in JavaScript.
+
+The switch statement is used to compare one value with many cases.
+
+Example:
+let day = 2;
+
+switch (day) {
+  case 1:
+    console.log("Monday");
+    break;
+  case 2:
+    console.log("Tuesday");
+    break;
+  default:
+    console.log("Other day");
+}
---
+++ switch/switch.js
@@ -0,0 +1,31 @@
+///if esle subsitute
+
+const month = 3
+
+switch (month) {
+    case 1:
+        console.log("January");
+        
+        break;
+    case 2:
+        console.log("FEB");
+        
+        break;
+    case 3:
+        console.log("MARCH");
+        
+        break;
+    case 4:
+        console.log("APRIL");
+        
+        break;
+    case 5:
+        console.log("MAY");
+        
+        break;
+
+    default:
+        console.log("are yr");
+        
+        break;
+}
